76a4524e80a57818c9a38e69801f9d93c23d1647
iamashutoshp <44366348+iamashutoshp@users.noreply.github.com>
2022-03-31 16:10:54 +0530

    dummy posts

---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+import { Button, Grid } from "@mui/material";
+
+const AboutMe = (props: any) => {
+  return (
+    <div>
+      <Grid container spacing={2} style={{marginTop:'123',marginRight:'auto'}}>
+        <Grid item xs={2} style={{marginTop:'3px'}}>
+          
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+export default AboutMe;
